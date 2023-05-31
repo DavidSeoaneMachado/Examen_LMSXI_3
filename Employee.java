80ba3e894c8c25c03bebeb87208d6e0ef6029bf1
David Seoane <114103657+DavidSeoaneMachado@users.noreply.github.com>
2023-05-31 13:03:55 +0200

    ejercicio 4 xquery

--- Employee.java
+++ Employee.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class employee {
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Employee {
+
+    private String first_name;
+    private String lastName;
+
+    public Employee(String first_name, String lastName) {
+        this.first_name = first_name;
+        this.lastName = lastName;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        return "{\"employees\":" + gson.toJson(this) + "}";
+    }
 }
--- Gestion_employees.java
+++ Gestion_employees.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class gestion_employees {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class Gestion_employees {
+    public static void main(String[] args) {
+        ArrayList<Employee> arrayList = new ArrayList<>();
+
+        try {
+            FileReader fileReader = new FileReader("employees.json");
+
+            // Leer el fichero JSON
+            Gson gson = new Gson();
+
+            // Obtener el tipo de datos para el ArrayList<Employee>
+            Type employeeListType = new TypeToken<ArrayList<Employee>>() {}.getType();
+
+            // Deserializar el JSON en un ArrayList<Employee>
+            arrayList = gson.fromJson(fileReader, employeeListType);
+
+            // Imprimir los empleados del ArrayList
+            for (Employee employee : arrayList) {
+                System.out.println("First Name: " + employee.getFirst_name());
+                System.out.println("Last Name: " + employee.getLastName());
+                System.out.println("--------------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
